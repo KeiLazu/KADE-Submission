3355288c2faddd8d2f6356221f1ea1138275c60b
Kennix Lazuardi <kennixlazuardi@gmail.com>
2018-11-10 08:23:44 +0700

    prepare merging

--- app/src/main/java/com/github/footballclubsubmission/ui/fragments/teams/presenter/TeamsPresenter.kt
+++ app/src/main/java/com/github/footballclubsubmission/ui/fragments/teams/presenter/TeamsPresenter.kt
@@ -1,8 +1,11 @@
 package com.github.footballclubsubmission.ui.fragments.teams.presenter
 
+import com.github.footballclubsubmission.ui.fragments.teams.interactor.TeamsMvpInteractor
+import com.github.footballclubsubmission.ui.fragments.teams.view.TeamsMvpView
+
 /**
  *  Created by Kei Lazu (Kennix Lazuardi) on 11/9/2018
  *  check https://github.com/KeiLazu for more
  */
-class TeamsPresenter {
+class TeamsPresenter<V: TeamsMvpView, I: TeamsMvpInteractor> {
 }
